17a746befb65221d0b63f8164476fc98fee1f140
Ralnikov Denis <dentreyt2909@gmail.com>
2023-03-18 08:59:05 +0300

    Задача выполнена 2

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
+    public Deth8 deth8;
+    @Autowired
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth8.toString();
     }
 }
